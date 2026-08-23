277f1c32a3fc9d2bc8c39daad15d59234cd6718c
elifozgen0520-debug <elifozgen0520@gmail.com>
2026-08-23 10:30:35 +0300

    Update sw.js

--- ofis-duyuru/public/sw.js
+++ ofis-duyuru/public/sw.js
@@ -7,11 +7,13 @@ self.addEventListener('push', (event) => {
   const options = {
     body: data.body,
     icon: '/icon-192.png',
-    // Android üst durum çubuğundaki beyaz kareyi şeffaf zil simgesine dönüştüren SVG
     badge: 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white"><path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.89 2 2 2zm6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2z"/></svg>',
     image: data.image || undefined,
+    // Web Push Standart Özel Ses Tanımı
+    sound: '/notify.wav',
     silent: false,
-    vibrate: [1000, 200, 1000, 200, 1000],
+    // HARD TİTREŞİM: 1 Saniye Titrer, 0.1 Saniye Durur (Tam 4 Şiddetli Darbe)
+    vibrate: [1000, 100, 1000, 100, 1000, 100, 1000],
     tag: data.id || 'duyuru-pwa',
     renotify: true,
     requireInteraction: true,
